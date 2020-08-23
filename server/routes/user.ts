bedfd7dbe902e1300750a55f6da654a218f13e54
pacho <pachobernal5@gmail.com>
2020-08-23 12:46:35 -0500

    Endpoint to add user funds

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -5,6 +5,8 @@ import _ from 'lodash';
 import ConnectionMysql from '../database/mysql';
 import short from 'short-uuid';
 import bcrypt from 'bcryptjs';
+import auth from 'express-jwt';
+import { config } from '../config';
 export default class User{
   public path = '/api/user';
   public router = Router();
@@ -13,6 +15,7 @@ export default class User{
   }
   public initializeRoutes() {
     this.router.post(`${this.path}/new`, this.newUser);
+    this.router.post(`${this.path}/addfunds`, auth({ secret: config.token.seed, algorithms: ['HS256'] }), this.addFunds);
   }
   newUser = async (req: Request, res: Response) => {
     try {
@@ -35,6 +38,24 @@ export default class User{
         return ServerResponse.message(res, 200, 0, 'El nombre de usuario ya existe');
       }
       return ServerResponse.message(res, 200, 1, 'Usuario creado exitosamente');
+    } catch (error) {
+      return ServerResponse.error(res, error.toString());
+    }
+  }
+  addFunds = async (req: Request, res: Response) => {
+    try {
+      const validateFields = ['funds'];
+      const validate = await Functions.formValidate(validateFields, req.body);
+      if (validate.error === 1) {
+        return ServerResponse.message(res, 200, 0, validate.message);
+      }
+      const { funds } = req.body;
+      if (typeof(funds) !== 'number') {
+        return ServerResponse.message(res, 200, 0, 'El campo funds debe ser númerico');
+      }
+      const userId = req[`user`].id;
+      const addFunds = await ConnectionMysql.queryWithParameters('CALL add_funds (?, ?)', [funds, userId]);
+      return ServerResponse.message(res, 200, 1, `Fondos agregados exitosamente, su nuevo saldo es $${addFunds[0].funds}`);
     } catch (error) {
       return ServerResponse.error(res, error.toString());
     }
